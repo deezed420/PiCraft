c8425fa2d8db236dbdaba6e39e21d33a326771af
Deezed420 <thepurplexity@gmail.com>
2024-06-29 23:57:59 -0400

    Skibidi

--- source/main.py
+++ source/main.py
@@ -1,11 +1,11 @@
 from core.progress import ProgressBar
 from core.menu import Menu
 
-from psutil import virtual_memory
+from psutil import virtual_memory, disk_usage, getloadavg
 from os import get_terminal_size
-from os.path import exists
 from requests import get
 from time import sleep
+import pathlib
 
 if get_terminal_size()[0] < 85: print("""
     ██████╗  ██████╗██╗  ██╗
@@ -14,6 +14,7 @@ if get_terminal_size()[0] < 85: print("""
     ██╔═══╝ ██║      ██╔██╗ 
     ██║     ╚██████╗██╔╝ ██╗
     ╚═╝      ╚═════╝╚═╝  ╚═╝""")
+
 else: print("""
 \x1b[38;5;196m██████╗ ██╗███╗   ██╗███████╗ ██████╗██████╗  █████╗ ███████╗████████╗\x1b[38;5;82m    ██╗  ██╗
 \x1b[38;5;196m██╔══██╗██║████╗  ██║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝\x1b[38;5;82m    ╚██╗██╔╝
@@ -25,8 +26,27 @@ else: print("""
 if get_terminal_size()[1] < 10: print('This terminal is too small!') ; exit()
 
 print('\n\nChecking system...')
+sleep(.5)
+
 RAM = round(virtual_memory()[3]/1000000000)
-if virtual_memory()[3]/1000000000 < 2: print('Warning: Less than 2GB RAM! There is little support for this. Expect lag and crashes.')
+if virtual_memory()[3]/1000000000 < 2: print('Warning: Less than 2GB RAM! Expect lag and crashes.')
 else: print('\nSystem has '+str(RAM)+'GB of RAM.')
 
-print('\nChecking')
+print('\nChecking files...')
+sleep(.2)
+scriptdir = str(pathlib.Path(__file__).parent)
+pathlib.Path(scriptdir+'/data').mkdir(parents=True, exist_ok=True)
+pathlib.Path(scriptdir+'/servers').mkdir(parents=True, exist_ok=True)
+
+print('\nLoading servers...')
+sleep(.5)
+
+servers = {}
+for path in pathlib.Path(scriptdir+'/servers').iterdir():
+    if path.is_dir():
+        for file in path.iterdir(): 
+            if file.is_file() and file.name == 'config.bbcf':
+                servers[path.name] = dict(line.strip().split('=') for line in file.read_text().split())
+                
+
+print(servers)
